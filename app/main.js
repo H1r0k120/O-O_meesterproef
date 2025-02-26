c58b2aca6ad14f4d8eea080300042de0d7920a09
ThatTygo <tygomarctenwolde@gmail.com>
2025-02-26 21:40:13 +0100

    j

---
+++ app/main.js
@@ -0,0 +1,15 @@
+/*
+Input = json array
+
+
+Output = HTML tabel
+
+
+Check naar tijd, link de tijd met de relatieve index, krijg de info, link die naar de html dinges
+*/
+
+
+const raw_table = loadStrings("schedule.txt")
+
+
+console.log(raw_table)

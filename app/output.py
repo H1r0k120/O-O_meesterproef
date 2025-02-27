620beacb127cecad1e46734a252a92007ffd5f4a
ThatTygo <tygomarctenwolde@gmail.com>
2025-02-27 11:44:17 +0100

    j

---
+++ app/output.py
@@ -0,0 +1,19 @@
+from jinja2 import Template
+
+html_template = """
+<!DOCTYPE html>
+<html>
+<head><title>Python Output</title></head>
+<body>
+    <h1>Python Output:</h1>
+    <p>{{ output }}</p>
+</body>
+</html>
+"""
+
+data = {"output": "Dit is een gegenereerde HTML met Python!"}
+template = Template(html_template)
+html_output = template.render(data)
+
+with open("output.html", "w", encoding="utf-8") as f:
+    f.write(html_output)
